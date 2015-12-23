3ca0d27fe6ff945e3d055dd1bb976910e496ca0e
Victor Boissiere <victor.boissiere@epita.fr>
2015-12-23 10:36:00 +0100

    Add logs table to admin security page

--- resources/views/admin/security/security.blade.php
+++ resources/views/admin/security/security.blade.php
@@ -7,6 +7,22 @@
 @endsection
 @section('page')
 <div class="row">
-  Coming soon
+  <div class="col-xs-12">
+    <div class="box">
+      <div class="box-header">
+        <h3 class="box-title">Logs</h3>
+      </div>
+      <div class="box-body table-responsive no-padding">
+        <table class="table table-hover">
+          <tr>
+            <th>ID</th>
+            <th>type</th>
+            <th>date</th>
+            <th>IP</th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
