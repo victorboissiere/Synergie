a68764ed45f9520dc4fa94bd655c677532d8bbc2
Victor Boissiere <victorboissiere@vps295473.ovh.net>
2016-07-05 02:08:37 +0200

    Update headers to match SSL url

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -183,7 +183,7 @@
     </div><!-- ./wrapper -->
 
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
     <script src="{{ asset('admin-theme/js/bootstrap.min.js') }}" type="text/javascript"></script>
 
     <script src="{{ asset('admin-theme/js/AdminLTE/app.js') }}" type="text/javascript"></script>
